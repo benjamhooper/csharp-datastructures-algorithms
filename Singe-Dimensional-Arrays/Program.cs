259e0b7c893af788a79b2fc7543132f1d11d7b46
Benjamin Hooper <me@benhooper.org>
2020-06-21 22:33:07 -0500

    Single Dimensional Arrays

---
+++ Singe-Dimensional-Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Singe_Dimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = new string[12];
+
+            for (int month = 1; month <= 12; month++)
+            {
+                DateTime firstDay = new DateTime(DateTime.Now.Year, month, 1);
+                string name = firstDay.ToString("MMMM", CultureInfo.CreateSpecificCulture("en"));
+
+                months[month -1] = name;
+            }
+
+            foreach(string month in months)
+            {
+                Console.WriteLine($"-> {month}");
+            }
+        }
+    }
+}
