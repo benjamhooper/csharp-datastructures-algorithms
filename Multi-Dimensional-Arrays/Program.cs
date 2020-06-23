990fdff896fcc58ce1fb43ac292326b6a157700f
Benjamin Hooper <me@benhooper.org>
2020-06-22 20:23:35 -0500

    Multi Dimensional Arrays

---
+++ Multi-Dimensional-Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Multi_Dimensional_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] results = new int[10, 10];
+
+            for (int i = 0; i < results.GetLength(0); i++)
+            {
+                for (int j = 0; j < results.GetLength(1); j++)
+                {
+                    results[i,j] = (i + 1) * (j + 1);
+                    Console.Write("{0,4}", results[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
